50a57a8fe9a0a1e537710939f806656916b94364
kat-nz <Kathryn@yazarin.nz>
2022-06-16 18:01:48 +1200

    add customers model

---
+++ models/customers.sql
@@ -0,0 +1,61 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+with customers as (
+
+    select
+        id as customerid,
+        firstname,
+        lastname
+
+    from public.customers
+
+),
+
+orders as (
+
+    select
+        id as orderid,
+        userid as customerid,
+        orderdate,
+        status
+
+    from public.orders
+
+),
+
+customer_orders as (
+
+    select
+        customerid,
+
+        min(orderdate) as first_order_date,
+        max(orderdate) as most_recent_order_date,
+        count(orderid) as number_of_orders
+
+    from orders
+
+    group by 1
+
+),
+
+final as (
+
+    select
+        customers.customerid,
+        customers.firstname,
+        customers.lastname,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    from customers
+
+    left join customer_orders using (customerid)
+
+)
+
+select * from final
